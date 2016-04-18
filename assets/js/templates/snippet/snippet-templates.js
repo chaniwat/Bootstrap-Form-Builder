d33987f28e4c1dcdbbddbd3cc17b2c64d4af1d4d
Chaniwat Seangchai <chaniwat.meranote@gmail.com>
2016-04-19 01:11:39 +0700

    edited .pop-over form (to bootstrap 3) / edited input tab form (to bootstrap 3) / remove unnecessary inputs

--- assets/js/templates/snippet/snippet-templates.js
+++ assets/js/templates/snippet/snippet-templates.js
@@ -3,18 +3,15 @@ define(function(require) {
   , prependedtext            = require('text!templates/snippet/prependedtext.html')
   , search                   = require('text!templates/snippet/searchinput.html')
   , textinput                = require('text!templates/snippet/textinput.html')
-  , appendedcheckbox         = require('text!templates/snippet/appendedcheckbox.html')
   , appendedtext             = require('text!templates/snippet/appendedtext.html')
   , filebutton               = require('text!templates/snippet/filebutton.html')
   , button                   = require('text!templates/snippet/button.html')
   , buttondouble             = require('text!templates/snippet/buttondouble.html')
-  , buttondropdown           = require('text!templates/snippet/buttondropdown.html')
   , multiplecheckboxes       = require('text!templates/snippet/multiplecheckboxes.html')
   , multiplecheckboxesinline = require('text!templates/snippet/multiplecheckboxesinline.html')
   , multipleradios           = require('text!templates/snippet/multipleradios.html')
   , multipleradiosinline     = require('text!templates/snippet/multipleradiosinline.html')
   , passwordinput            = require('text!templates/snippet/passwordinput.html')
-  , prependedcheckbox        = require('text!templates/snippet/prependedcheckbox.html')
   , prependedtext            = require('text!templates/snippet/prependedtext.html')
   , searchinput              = require('text!templates/snippet/searchinput.html')
   , selectbasic              = require('text!templates/snippet/selectbasic.html')
@@ -27,23 +24,18 @@ define(function(require) {
     , prependedtext            : prependedtext
     , search                   : search
     , textinput                : textinput
-    , appendedcheckbox         : appendedcheckbox
     , appendedtext             : appendedtext
     , filebutton               : filebutton
     , singlebutton             : button
     , doublebutton             : buttondouble
-    , buttondropdown           : buttondropdown
     , multiplecheckboxes       : multiplecheckboxes
     , multiplecheckboxesinline : multiplecheckboxesinline
     , multipleradios           : multipleradios
     , multipleradiosinline     : multipleradiosinline
     , passwordinput            : passwordinput
-    , prependedcheckbox        : prependedcheckbox
-    , prependedtext            : prependedtext
     , searchinput              : searchinput
     , selectbasic              : selectbasic
     , selectmultiple           : selectmultiple
     , textarea                 : textarea
-    , textinput                : textinput
   }
 });
